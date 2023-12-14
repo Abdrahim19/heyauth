fe6ae62e909a71ec0af6d61852af935d288cbac2
hamzaAbdrahim <rsstrr7@gmail.com>
2023-12-14 15:15:12 +0100

    'hamza'

---
+++ HTML/config/firbase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAsO8amTJnzVndoz0uRfVlGifgHDE0mUkM",
+  authDomain: "heyauth-4a3ab.firebaseapp.com",
+  projectId: "heyauth-4a3ab",
+  storageBucket: "heyauth-4a3ab.appspot.com",
+  messagingSenderId: "943518805455",
+  appId: "1:943518805455:web:5b387e0e94d55fa572f6ba"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
